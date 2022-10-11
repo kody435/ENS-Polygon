0eb3457d4dd900199062288d3ff2e6b5d011cb93
Cody <bittusmaths@gmail.com>
2022-10-11 18:20:32 +0530

    Add files via upload

---
+++ contract/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: "https://crimson-side-energy.matic-testnet.discover.quiknode.pro/21a62fa28cca3231fdbc4f8453352acd0dd65aaf/",
+      accounts: ["70e780214a83ef51d4156a6724ada1f85ec25926ccb921a36256a000d3cbfd16"],
+    },
+  },
+};
